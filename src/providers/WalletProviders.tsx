1898707942ed9ac673b393e06780a2553a440ff0
0leodev <leojimdev@gmail.com>
2025-10-17 18:38:24 -0400

    improved readability for WalletProviders

--- src/providers/WalletProviders.tsx
+++ src/providers/WalletProviders.tsx
@@ -1,22 +1,19 @@
 "use client";
-import React from "react";
-import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
-import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
+import { config } from "@/config/wagmiConfig";
 import "@rainbow-me/rainbowkit/styles.css";
-import { config } from '@/config/wagmiConfig';
 
 const queryClient = new QueryClient();
 
-export default function Providers({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider theme={darkTheme()}>{children}</RainbowKitProvider>
+        <RainbowKitProvider theme={darkTheme()}>
+          {children}
+        </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
